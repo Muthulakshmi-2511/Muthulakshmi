8a992bb3a2b06444d837738d4744fc650c855630
Muthulakshmi-2511 <124550909+Muthulakshmi-2511@users.noreply.github.com>
2023-02-08 12:28:41 +0530

    Add files via upload

---
+++ Mobile.java
@@ -0,0 +1,26 @@
+package week1.dya1;
+
+public class Mobile {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		String mobileBrandName= "one plus";
+		char mobileLogo= '$';
+		Short noOfMobilePiece=2;
+		int modelNumber= 4325;
+		long mobilemeiNumber= 45237649;
+		boolean isDamaged=false;
+		System.out.println("mobile brand name- "+mobileBrandName);
+		System.out.println("mobile logo -"+mobileLogo);
+		System.out.println("Number of mobile pieces -"+noOfMobilePiece);
+		System.out.println("Model Number -"+modelNumber);
+		System.out.println("Mobile mei number -"+mobilemeiNumber);
+		System.out.println("is Damaged -"+isDamaged);
+		
+		
+		
+		
+	
+	}
+
+}
